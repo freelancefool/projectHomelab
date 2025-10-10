84732d6919f0757727c40f3c12d783ed730bc0fd
John Smith <freelancefool+gh@gmail.com>
2025-10-10 19:16:34 -0400

    Update: Rename to start/stop

---
+++ splunk/splunk_start.sh
@@ -0,0 +1,13 @@
+docker run -d \
+-e SPLUNK_START_ARGS=--accept-license \
+-e SPLUNK_GENERAL_TERMS=--accept-sgt-current-at-splunk-com \
+-e SPLUNK_PASSWORD=changeme \
+-p 8000:8000 \
+-p 8088:8088 \
+-p 8089:8089 \
+-p 9997:9997 \
+--volume /mnt/splunk/etc:/opt/splunk/etc \
+--volume /mnt/splunk/var:/opt/splunk/var \
+-h splunk.home.freelancefool.com \
+--name splunk \
+splunk/splunk:10.0.1-rhel9
