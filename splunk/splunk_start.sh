c89095205e0abe52e2c93c0387d4164b2e4d2f69
John Smith <freelancefool+gh@gmail.com>
2025-10-10 20:15:30 -0400

    Fix: Set a license.

--- splunk/splunk_start.sh
+++ splunk/splunk_start.sh
@@ -2,6 +2,7 @@ docker run -d \
 -e SPLUNK_START_ARGS=--accept-license \
 -e SPLUNK_GENERAL_TERMS=--accept-sgt-current-at-splunk-com \
 -e SPLUNK_PASSWORD=changeme \
+-e SPLUNK_LICENSE_URI=http://apache.home.freelancefool.com/Splunk.License \
 -p 8000:8000 \
 -p 8088:8088 \
 -p 8089:8089 \
