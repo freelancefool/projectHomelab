62aa02598a331a56c9c9cea2a574da176145cc87
John Smith <freelancefool+gh@gmail.com>
2025-10-10 22:22:47 -0400

    Fix: Properly mapped arguments

--- sc4s/sc4s_start.sh
+++ sc4s/sc4s_start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
-docker run -it \
+docker run -d \
 -p 514:514/tcp \
 -p 514:514/udp \
---volume ./env_file:/opt/sc4s/env_file \
---volume /mnt/sc4s/local:/opt/sc4s/local \
---volume /mnt/sc4s/archive:/opt/sc4s/archive \
---volume /mnt/sc4s/tls:/opt/sc4s/tls \
+--volume /mnt/sc4s/persistent:/var/lib/syslog-ng \
+--volume /mnt/sc4s/local:/etc/syslog-ng/conf.d/local:z \
+--volume /mnt/sc4s/archive:/var/lib/syslog-ng/archive:z \
+--volume /mnt/sc4s/tls:/etc/syslog-ng/tls:z \
+--env-file=./env_file \
 -h sc4s.home.freelancefool.com \
 --name sc4s \
 ghcr.io/splunk/splunk-connect-for-syslog/container3:v3.38.0
